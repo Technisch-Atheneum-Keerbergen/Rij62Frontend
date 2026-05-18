6a609cc87f41448debe9105a75755e3da2936842
frickingbird8002 <sakul8002@icloud.com>
2026-05-18 17:04:41 +0200

    prep for orderCard

---
+++ src/routes/admin/orders/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { apiFetch } from '$lib/api/client';
+	import type { Order } from '$lib/api/types/order';
+	async function getAllOrders() {
+		return (await apiFetch('/orders')) as Order[];
+	}
+</script>
+
+{#await getAllOrders()}
+	Loading...
+{:then orders}
+	{console.log(orders)}
+{/await}
