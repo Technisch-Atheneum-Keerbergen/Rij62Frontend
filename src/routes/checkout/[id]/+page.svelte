b427ab673acf0bb2f9a50fdce49ca6535ada80a4
frickingbird8002 <sakul8002@icloud.com>
2026-05-22 11:54:56 +0200

    prep for checkout page

---
+++ src/routes/checkout/[id]/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import type { OrderId } from '$lib/api/types/order';
+	import { page } from '$app/state';
+
+	const orderId: OrderId = page.params.id as OrderId;
+	console.log(orderId);
+</script>
